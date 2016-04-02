714956b017ba126e2fdbaefb2b16303980ffcc9a
Alex Egan <alexander.egan@gmail.com>
2016-04-02 10:28:24 -0400

    add some basic tests for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :email, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :username, presence: true,
+                       uniqueness: { case_sensitive: false },
+                       format: { with: /\A@?(\w){1,15}$\z/ }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    invalid_user = User.new
+    invalid_user.valid?
+    @errors = invalid_user.errors
+  end
+
+  test 'should have invalid email' do
+    assert_not_empty @errors[:email]
+  end
+
+  test 'should have invalid password' do
+    assert_not_empty @errors[:password]
+  end
+
+  test 'should have invalid username' do
+    assert_not_empty @errors[:username]
+  end
 end
